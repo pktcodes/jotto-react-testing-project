aa3dc407b21b300fa9b145b8b56564b3656a4f16
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-11-02 22:30:12 -0500

    Code the Congrats Component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,5 +1,19 @@
-const Congrats = () => {
-  return <div></div>;
+/**
+ * Functional react component for Congratulatory message.
+ * @function
+ * @param {object} props - React props
+ * @returns {JSX.Element} - Rendered message (or null if `success` prop is false
+ */
+const Congrats = ({ success }) => {
+  return (
+    <div data-test="congrats-component">
+      {success ? (
+        <div data-test="congrats-message">
+          Congratulations! You have guessed the word!
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Congrats;
