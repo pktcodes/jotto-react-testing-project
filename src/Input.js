3e621fc32fd903bd25cfacf8e00d8977ea7a79f7
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-11-15 23:11:00 -0600

    Input Component Props

--- src/Input.js
+++ src/Input.js
@@ -1,5 +1,11 @@
-const Input = () => {
+import PropTypes from 'prop-types';
+
+const Input = ({ secretWord }) => {
   return <div data-test="input-component"></div>;
+};
+
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
 };
 
 export default Input;
--- src/Input.test.js
+++ src/Input.test.js
@@ -1,7 +1,11 @@
 import { shallow } from 'enzyme';
 
-import { findByTestAttribute } from '../test/utils';
+import { findByTestAttribute, validateProps } from '../test/utils';
 import Input from './Input';
+
+const defaultProps = {
+  secretWord: 'party',
+};
 
 /**
  * Factory function to create a ShallowWrapper for the Input component
@@ -9,12 +13,18 @@ import Input from './Input';
  * @param {object} props - Component props specific to this setup
  * @returns {ShallowWrapper}
  */
-const setup = () => {
-  return shallow(<Input />);
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 test('renders input component without error', () => {
   const wrapper = setup();
   const inputComponent = findByTestAttribute(wrapper, 'input-component');
   expect(inputComponent.length).toBe(1);
+});
+
+test('check the expected props', () => {
+  const expectedProps = { ...defaultProps };
+  validateProps(Input, expectedProps);
 });
