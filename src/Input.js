8be57814d5e587f90d815ae900676a0c3afa6945
Praveen Kumar <praveenkumarthipparam@gmail.com>
2024-12-21 15:05:40 -0600

    Prepare Input Component for useState Tests

--- src/Input.js
+++ src/Input.js
@@ -1,7 +1,32 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const Input = ({ secretWord }) => {
-  return <div data-test="input-component"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  return (
+    <div data-test="input-component">
+      <form className="d-inline-flex gap-3">
+        <input
+          data-test="input-box"
+          type="text"
+          className="mb-2 rounded form-control"
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(event) => {
+            return setCurrentGuess(event.target.value);
+          }}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 Input.propTypes = {
